9dfe1337322af76f5e61d3875f25317f8bfd8ba4
cleberspirlandeli <contato.cleberrezende@gmail.com>
2020-01-02 00:21:56 -0300

    continuando rest api

---
+++ app/app/Controllers/EditorasController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using app.Data;
+using app.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace App.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EditorasController : ControllerBase
+    {
+        private readonly BibliotecaContext _context;
+
+        public EditorasController(BibliotecaContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Editora>>> GetAll()
+        {
+            return await _context.Editoras.ToListAsync();
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Editora>> GetById(int id)
+        {
+            var editora = await _context.Editoras.FindAsync(id);
+
+            if (editora == null)
+                return NotFound();
+
+            return editora;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Insert(Editora editora)
+        {
+            _context.Editoras.Add(editora);
+            await _context.SaveChangesAsync();
+
+            return Ok(editora);
+        }
+
+    }
+}
--- app/app/Data/Map/EditoraMap.cs
+++ app/app/Data/Map/EditoraMap.cs
@@ -1,10 +1,6 @@
 ﻿using app.Models;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace App.Data.Map
 {
